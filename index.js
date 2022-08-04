0b1740980addad46c8e21708fde21fadc2051ae6
Gledson Leal <gledsonleal@ifsul.edu.br>
2022-08-04 16:50:29 -0300

    API com framework Express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+/**
+cors - Compartilhamento de Recursos de Origem Cruzada
+    
+    - corrige os problemas de requisições sendo enviadas
+        para o mesmo domínio. Quando estamos rodando a API
+        e o front-end, o navegador poderá bloquear. 
+        O Cors realiza esse desvio a proteção que existe 
+        no próprio navegador
+ */
+app.use(express.json())
+//app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))// porta 3000 front end
+app.use(cors({ credentials: true, origin: 'http://localhost:8080' }))// porta 3000 front end - Vuejs
+//app.use(express.static('public'))
+//rotas
+const AdminRoutes = require('./routes/AdminRoutes')
+app.use('/', AdminRoutes)
+//servidor
+app.listen(5000,()=>{
+    console.log(`API rodando na porta 5000`)
+})
